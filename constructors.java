4e36335aea1b5403cca47c07be486dd38a0ba68d
Anshul kumar <81064196+Anshul9602@users.noreply.github.com>
2022-04-22 11:00:49 +0530

    Add files via upload

---
+++ constructors.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class MyMainEmployee {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class constructors {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+
+    }
+}
